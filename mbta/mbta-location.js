764e570f5639f54eb0b069e3edfec22d534703ba
jmoral01 <27jessicam@gmail.com>
2016-10-19 10:19:41 -0400

    modified to show current location with marker

--- mbta/mbta-location.js
+++ mbta/mbta-location.js
@@ -1,7 +1,40 @@
-var map
+var map;
+var marker;
+var myLat = 0;
+var myLng = 0;
+var myLocation;
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 42.39674, lng:-71.121815}, zoom: 8
-	});
+var myOptions;
+
+function init() {
+	myLocation = new google.maps.LatLng(myLat, myLng);
+	myOptions = {
+		zoom: 10,
+		center:{lat: 42.39674, lng: -71.121815},
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	getLocation();
 }
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			displayLocation();
+		});
+	} else {
+		alert("Browser does not support geolocation");
+	}	
+}
+
+function displayLocation() {
+	myLocation = new google.maps.LatLng(myLat, myLng);
+	map.panTo(myLocation);
+	marker = new google.maps.Marker({
+		position: myLocation
+	});
+	marker.setMap(map);
+}
+
